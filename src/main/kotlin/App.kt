8768ee566bb3593e3e358937315c58aead688782
KYokoyama <8509057+KaiseiYokoyama@users.noreply.github.com>
2021-09-06 00:06:17 +0900

    #37 コレクションにならないフォルダをドロップされたらダイアログを出す

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,10 +1,9 @@
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.FolderOpen
+import androidx.compose.material.icons.filled.Photo
 import androidx.compose.runtime.*
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.Composable
@@ -13,11 +12,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.awt.ComposeWindow
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import content.Collection
+import content.CollectionException
+import content.CollectionLoadException
+import content.NoMediaCollectionException
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import java.awt.datatransfer.DataFlavor
 import java.awt.dnd.DnDConstants
@@ -25,12 +29,63 @@ import java.awt.dnd.DropTarget
 import java.awt.dnd.DropTargetDropEvent
 import java.io.File
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun App(composeWindow: ComposeWindow) {
     var rootCollection: Collection? by remember { mutableStateOf(null) }
     // ロード中か否か
-    var loading : Boolean by remember { mutableStateOf(false) }
+    var loading: Boolean by remember { mutableStateOf(false) }
     val coroutineScope = rememberCoroutineScope()
+
+    // CollectionLoadExceptionDialog
+    var collectionLoadExceptionDialog: Boolean by remember { mutableStateOf(false) }
+    if (collectionLoadExceptionDialog)
+        AlertDialog(
+            modifier = Modifier.width(400.dp),
+            onDismissRequest = { collectionLoadExceptionDialog = false },
+            title = {
+                Row(horizontalArrangement = Arrangement.Start) {
+                    Icon(Icons.Default.FolderOpen, contentDescription = "フォルダをドロップしてください")
+                    Text("フォルダをドロップしてください")
+                }
+            },
+            text = { Text("コレクションとしてロードできるのはフォルダのみです") },
+            buttons = {}
+        )
+    // NoMediaCollectionExceptionDialog
+    var noMediaCollectionExceptionDialog: Boolean by remember { mutableStateOf(false) }
+    if (noMediaCollectionExceptionDialog)
+        AlertDialog(
+            modifier = Modifier.width(400.dp),
+            onDismissRequest = { noMediaCollectionExceptionDialog = false },
+            title = {
+                Row(horizontalArrangement = Arrangement.Start) {
+                    Box(contentAlignment = Alignment.Center) {
+                        Icon(
+                            Icons.Default.Photo,
+                            contentDescription = "メディア",
+                            Modifier.graphicsLayer {
+                                scaleX = 0.9f
+                                scaleY = 0.9f
+                            },
+                            tint = MaterialTheme.colors.onSurface.copy(alpha = 0.6f)
+                        )
+                        Icon(
+                            Icons.Default.Clear,
+                            contentDescription = "なし",
+                            Modifier.graphicsLayer {
+                                scaleX = 1.3f
+                                scaleY = 1.3f
+                            },
+                            tint = MaterialTheme.colors.error
+                        )
+                    }
+                    Text("メディアが入っていません")
+                }
+            },
+            text = { Text("Valokniteが表示できるメディアがコレクションに入っていませんでした") },
+            buttons = {}
+        )
 
     if (rootCollection != null) {
         ViewerContainer(ViewerContainerState.new(rootCollection!!))
@@ -41,7 +96,17 @@ fun App(composeWindow: ComposeWindow) {
             // ロードに移行したことをユーザに示す
             loading = true
             coroutineScope.launch {
-                rootCollection = Collection.new(it.toPath())
+                rootCollection = try {
+                    Collection.new(it.toPath())
+                } catch (e: CollectionLoadException) {
+                    // フォルダでないものを読み込んだとき
+                    collectionLoadExceptionDialog = true
+                    null
+                } catch (e: NoMediaCollectionException) {
+                    // メディアの入ってないフォルダを読み込んだとき
+                    noMediaCollectionExceptionDialog = true
+                    null
+                }
                 // ロードを終えたことをユーザに示す
                 loading = false
             }
@@ -55,7 +120,7 @@ fun App(composeWindow: ComposeWindow) {
 @Composable
 fun FileDroppableArea(composeWindow: ComposeWindow, onDropped: (File) -> Unit) {
     // ファイルをドロップしてください的なメッセージ
-    Surface (contentColor = MaterialTheme.colors.onBackground.copy(alpha = 0.6f)) {
+    Surface(contentColor = MaterialTheme.colors.onBackground.copy(alpha = 0.6f)) {
         Box(
             Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center,
@@ -97,7 +162,7 @@ fun FileDroppableArea(composeWindow: ComposeWindow, onDropped: (File) -> Unit) {
  */
 @Composable
 fun LoadingScreen() {
-    Surface (contentColor = MaterialTheme.colors.onBackground.copy(alpha = 0.6f)) {
+    Surface(contentColor = MaterialTheme.colors.onBackground.copy(alpha = 0.6f)) {
         Box(
             Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center,
