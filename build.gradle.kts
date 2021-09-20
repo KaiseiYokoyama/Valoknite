6cc6d5cfaa0cde6fd05e4a0b8aa8b62339e9226d
KYokoyama <8509057+KaiseiYokoyama@users.noreply.github.com>
2021-09-20 15:34:19 +0900

    refactor: 新モジュール PxParserを切り出し

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.30"
-    kotlin("plugin.serialization") version "1.5.30"
+    kotlin("jvm") version "1.5.21"
     id("org.jetbrains.compose") version "1.0.0-alpha3"
 }
 
